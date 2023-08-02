5a07dd98001f7cd39812a87baf44171eaf7b3e45
NicolasDeniau <nicolas.deniau@oclock.school>
2023-08-02 16:09:07 +0200

    reboot vm

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 (function () {
+    const isLightString = localStorage.getItem('light-mode');
+    if (isLightString !== null) {
+        const isLight = JSON.parse(isLightString);
+        document.body.classList.toggle("light-mode", isLight);
+    }
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
@@ -9,5 +14,18 @@
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
+        saveToLocalStorage();
     })
 })();
+function saveToLocalStorage () {
+    // On vérifie si le body contient le thème lumineux
+    // classList.contains() indique si l'élément contient une classe (true) ou non
+    const isLight = document.querySelector('body').classList.contains('light-mode');
+
+    // On transforme la valeur booléenne en chaîne de caractères pour garantir 
+    // le bon enregistrement des données
+    const isLightString = JSON.stringify(isLight);
+
+    // On sauvegarde l'activation du thème lumineux dans le localStorage
+    localStorage.setItem('light-mode', isLightString);
+}
